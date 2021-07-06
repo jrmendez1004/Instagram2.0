3bcf3dd086b6226eb11ac7144af6528deace7c8a
jrmendez1004 <86307817+jrmendez1004@users.noreply.github.com>
2021-07-06 17:10:15 -0500

    Login page created

---
+++ app/src/main/java/com/example/instagram20/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.instagram20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etPassword = findViewById(R.id.tvPassword);
+        etUsername = findViewById(R.id.tvUsername);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
+
+    }
+}

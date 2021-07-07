00dd641717a68c43025e559d326ae2d6fa353acd
jrmendez1004 <86307817+jrmendez1004@users.noreply.github.com>
2021-07-07 14:46:54 -0500

    Login Working

--- app/src/main/java/com/example/instagram20/LoginActivity.java
+++ app/src/main/java/com/example/instagram20/LoginActivity.java
@@ -2,10 +2,16 @@ package com.example.instagram20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,6 +24,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        if(ParseUser.getCurrentUser() != null)
+            goMainActivity();
 
         etPassword = findViewById(R.id.tvPassword);
         etUsername = findViewById(R.id.tvUsername);
@@ -33,6 +42,20 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUser(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "issue with login", e);
+                    return;
+                }
+                goMainActivity();
+            }
+        });
+    }
 
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }

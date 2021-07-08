32eb4b7d78a3bfa22da83e523c3e9034b89dbafe
jrmendez1004 <86307817+jrmendez1004@users.noreply.github.com>
2021-07-08 16:20:46 -0500

    Detail View done

--- app/src/main/java/com/example/instagram20/PostDetailActivity.java
+++ app/src/main/java/com/example/instagram20/PostDetailActivity.java
@@ -2,6 +2,7 @@ package com.example.instagram20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.widget.ImageView;
@@ -17,6 +18,7 @@ public class PostDetailActivity extends AppCompatActivity {
     TextView tvUsername;
     TextView tvDetail;
     ImageView ivPost;
+    TextView tvTime;
     Post post;
 
     @Override
@@ -27,11 +29,13 @@ public class PostDetailActivity extends AppCompatActivity {
         tvUsername = (TextView) findViewById(R.id.tvUsername);
         tvDetail = (TextView) findViewById(R.id.tvDescription);
         ivPost = (ImageView) findViewById(R.id.ivImage);
+        tvTime = (TextView) findViewById(R.id.tvTime);
 
         post = Parcels.unwrap(getIntent().getParcelableExtra("post"));
 
         tvUsername.setText(post.getUser().getUsername());
         tvDetail.setText(post.getDescription());
+        tvTime.setText(Post.calculateTimeAgo(post.getCreatedAt()));
         ParseFile image = post.getImage();
         if (image != null) {
             Glide.with(this).load(image.getUrl()).into(ivPost);
